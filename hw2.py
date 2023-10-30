2ea8f2fc5a6769994bc8138d5cbbdfddc2b941f1
Monah-Haidar <mnhaidar01@gmail.com>
2023-10-30 14:13:51 +0200

    add running time of several functions and add comments

--- hw2.py
+++ hw2.py
@@ -1,4 +1,5 @@
-def menu():
+# Display Menu
+def menu():                           # O(1)
     print("""
           1. Add Matrices
           2. Check Rotation
@@ -9,10 +10,12 @@ def menu():
           7. Exit
           """)
           
+              
 # Funciton to create matrix 1
-def createMat1(rows, columns):
+def createMat1(rows, columns):       # O(N^2)
     mat1 = []
     for i in range(rows):
+        # declare an empty row
         lst = []
         for j in range(columns):
             elem = int(input(f"Enter elements of the {i+1} row of the first matrix: "))
@@ -20,10 +23,12 @@ def createMat1(rows, columns):
         mat1.append(lst)    
     return mat1
 
+
 # Funciton to create matrix 2
-def createMat2(rows, columns):
+def createMat2(rows, columns):       # O(N^2)
     mat2 = []
     for i in range(rows):
+      # declare an empty row
       lst = []
       for j in range(columns):
           elem = int(input(f"Enter elements of the {i+1} row of the second matrix: "))
@@ -33,17 +38,20 @@ def createMat2(rows, columns):
     
     
     
+    
 
 def main():
-    # name = input("Please Enter Your Name: ")
-    # print(f"Hello {name}")
+    # Ask the user for their name and welcome them
+    name = input("Please Enter Your Name: ")
+    print(f"Hello {name}")
     
-    # print(menu())
+    # Print the menu
+    print(menu())
     
-    # user_input = input("Please choose a number from the above menu: ")
-    # print("You chose:", user_input)
+    # Ask user to choose a number from the menu
+    user_input = input("Please choose a number from the above menu: ")
+    print("You chose:", user_input)
 
-    user_input = 1 
     
     if user_input == 1:
         # Ask user to enter rows and columns
@@ -52,10 +60,15 @@ def main():
        
         mat1 = createMat1(rows, columns)
         mat2 = createMat2(rows, columns)
-     
-
- 
+        mat3 = []
         
+        for i in range(rows):
+            # initialize each row of mat3 in order to assign values in the 2nd for loop
+            mat3.append([0] * columns)
+            for j in range(columns):
+                mat3[i][j] = mat1[i][j] + mat2[i][j]
+ 
+        print(mat3)
 
 
 
