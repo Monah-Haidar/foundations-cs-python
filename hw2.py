b78fc657557d151944bee38d67b50e9fd15d6c53
Monah-Haidar <mnhaidar01@gmail.com>
2023-10-30 12:57:43 +0200

    added menu function

---
+++ hw2.py
@@ -0,0 +1,18 @@
+def menu():
+    print("""
+          1. Add Matrices
+          2. Check Rotation
+          3. Invert Dictionary
+          4. Convert Matrix to Dictionary
+          5. Check Palindrome
+          6. Search for an Element & Merge Sort
+          7. Exit
+          """)
+          
+
+
+def main():
+    print(menu())
+    
+
+main()
