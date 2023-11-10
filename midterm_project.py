f5aa652ee98d43ab33d63b2bc5978b361b8c9ceb
Monah-Haidar <mnhaidar01@gmail.com>
2023-11-10 12:25:47 +0200

    Greet the user

---
+++ midterm_project.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    # Prompt the user to enter their name and greet them
+    user_name = input("Enter your name: ")
+    print(f"Welcome to our Advanced Browser {user_name}")
+    
+    
