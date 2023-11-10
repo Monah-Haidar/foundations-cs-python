82a28c1a7e776d7ba88ed768c4b09024973303e4
Monah-Haidar <mnhaidar01@gmail.com>
2023-11-10 12:36:50 +0200

    Add Diplay Menu Function and take user input

--- midterm_project.py
+++ midterm_project.py
@@ -1,7 +1,19 @@
 
 
-
-
+# Function to display the menu
+# Running Time: O(1)
+def displayMenu():
+    print("""
+          1. Open Tab
+          2. Close Tab
+          3. Switch Tab
+          4. Display All Tabs
+          5. Open Nested Tab
+          6. Clear All Tabs
+          7. Save Tabs
+          8. Import Tabs
+          9. Exit
+          """)
 
 
 
@@ -14,4 +26,16 @@ def main():
     user_name = input("Enter your name: ")
     print(f"Welcome to our Advanced Browser {user_name}")
     
+    # Display Menu
+    displayMenu()
     
+    # Take user choice 
+    user_input = int(input("Please choose a number from the menu above: "))
+    print(f"\nYou chose {user_input}")
+    
+    
+    
+    
+    
+    
+main()
